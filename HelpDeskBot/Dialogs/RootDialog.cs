6f7a30aa7c8bfa4f748b5f00cfb7f3318b4c99c0
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-02-07 10:25:19 +0900

    それっぽい受け答え

--- HelpDeskBot/Dialogs/RootDialog.cs
+++ HelpDeskBot/Dialogs/RootDialog.cs
@@ -8,11 +8,23 @@ namespace HelpDeskBot.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        private string category;
+        private string severity; // 厳しさ
+        private string description;
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
 
             return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("Help Desk Bot です。サポートデスク受付チケットの発行を行います。");
+
+            PromptDialog.Text(context: context, resume: this.DescriptionMessageReceivedAsync, prompt: "どんなことにお困りですか？");
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
@@ -26,6 +38,13 @@ namespace HelpDeskBot.Dialogs
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task DescriptionMessageReceivedAsync(IDialogContext context, IAwaitable<string> argument)
+        {
+            this.description = await argument;
+            await context.PostAsync($"承知しました。内容は \"{this.description}\" ですね。");
+            context.Done<object>(value: null);
         }
     }
 }
