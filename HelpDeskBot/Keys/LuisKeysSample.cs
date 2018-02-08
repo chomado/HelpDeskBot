d91fddc7dd29fb6eb77a34a2cefc657f0f31e23c
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-02-08 10:02:47 +0900

    LUIS keys

---
+++ HelpDeskBot/Keys/LuisKeysSample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDeskBot.Keys
+{
+    public static class LuisKeysSample
+    {
+        public static readonly string AppID = "";
+        public static readonly string SubscriptionKey = "";
+    }
+}
